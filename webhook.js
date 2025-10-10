5239051e6fe232098b764615a5d2f8bdd8e07df4
Tells Fox (Silent masonwolf) <silentmason1@gmail.com>
2025-10-10 12:54:39 -0400

    Added the ability to edit a message by the bot

--- webhook.js
+++ webhook.js
@@ -1,18 +1,19 @@
  document.getElementById('webhook-button').addEventListener('click', function() {
+ // Get all of the things to get for all of the elements
  const webhookUrl = document.getElementById('webhook-url').value;
+ const messageId = document.getElementById('message-id').value;
  const message = document.getElementById('webhook-message').value;
  const title = document.getElementById('webhook-title').value;
  const imageUrl = document.getElementById('webhook-image-url').value;
  const authorName = document.getElementById('webhook-author').value;
  const color = document.getElementById('webhook-color').value;
  const colorInt = parseInt(color.slice(1), 16);
-
-
+ // Construct the data to post on discord
  const data = {
  embeds: [{
  title: title,
  description: message,
- color: colorInt, // The part that was changed
+ color: colorInt, // Purple color code
  author: {
  name: authorName
  },
@@ -21,13 +22,15 @@
  }
  }]
  };
+ // Construct it such that if there is a message, to edit it
+ const editMessageUrl = `${webhookUrl}/messages/${messageId}`;
 
- fetch(webhookUrl, {
- method: 'POST',
+ fetch(editMessageUrl, {
+ method: 'PATCH',
  headers: {
  'Content-Type': 'application/json'
  },
- body: JSON.stringify(data)
+ body: JSON.stringify({ embeds: data.embeds })
  })
  .then(response => {
  if (response.ok) {
