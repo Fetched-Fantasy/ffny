cefd81ccbb365e9951621dd9cd4f553fa81819a2
Tells Fox (Silent masonwolf) <silentmason1@gmail.com>
2025-10-16 08:19:33 -0400

    Vue

---
+++ sea.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- Your content here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sea',
+  components: {},
+  data() {
+    return {};
+  },
+  mounted() {
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
