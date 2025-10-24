9164f34edc717df7e07364146ad019b3fe65be16
Tells Fox (Silent masonwolf) <silentmason1@gmail.com>
2025-10-23 20:43:13 -0400

    nice

--- editor.js
+++ editor.js
@@ -30,3 +30,7 @@ const videoUpload = document.getElementById('videoUpload');
         timeline.addEventListener('input', function() {
             videoPlayer.currentTime = timeline.value;
         });
+var canvas = document.getElementById("myCanvas");
+        var ctx = canvas.getContext("2d");
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(0,0,150,75);
